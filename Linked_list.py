f46d3a45af60d1e1fa0df98c100aeeac55f54527
Nur Kholis Yusuf Rabbani <nurkholis.23074@mhs.unesa.ac.id>
2024-03-07 19:16:06 +0700

    Create Linked_list.py

---
+++ Linked_list.py
@@ -0,0 +1,63 @@
+import time
+
+class Node:
+    def __init__(self, menu, price, quantity=1):
+        self.menu = menu  #Nama menu
+        self.price = price  #Harga menu
+        self.quantity = quantity  #Jumlah pesanan
+        self.next = None  #Pointer ke node berikutnya
+
+class LinkedList:
+    def __init__(self):
+        self.head = None  #Inisialisasi kepala linked list
+
+    def add_menu(self, menu, price, quantity=1):
+        new_node = Node(menu, price, quantity)  #Membuat node baru
+        if not self.head:
+            #Menjadikan node baru sebagai kepala jika linked list masih kosong
+            self.head = new_node
+
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node  #Menambahkan node baru di belakang linked list
+
+    def display_menu(self):
+        current = self.head
+        count = 1
+        while current:
+            #Menampilkan menu, jumlah, dan total harga
+            print(f"{count}. {current.menu} ({current.quantity} pcs) - {current.price * current.quantity} rupiah")
+            current = current.next
+            count += 1
+
+    def calculate_total_price(self):
+        total_price = 0
+        current = self.head
+        while current:
+            total_price += current.price * current.quantity  # Menjumlahkan total harga pesanan
+            current = current.next
+        return total_price
+
+#Daftar menu beserta harga
+menu_dict = {
+    "miexue ice cream": 5000,
+    "boba shake": 16000,
+    "mi sundae": 14000,
+    "mi ganas": 11000,
+    "creamy mango boba": 22000
+}
+
+#Inisialisasi linked list untuk menyimpan pesanan
+menu_list = LinkedList()
+
+#Fungsi untuk memesan menu dan menambahkannya ke dalam keranjang
+def pesan_menu(menu, quantity=1):
+    menu_lower = menu.lower()
+    if menu_lower in menu_dict:
+        menu_list.add_menu(menu, menu_dict[menu_lower], quantity)  #Menambahkan menu ke dalam keranjang
+        print(f"{quantity} {menu} telah ditambahkan ke keranjang")
+
+    else:
+        print("Maaf, menu tidak tersedia")
